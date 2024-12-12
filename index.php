b688923f5db4e41cdebcf0548445485bf360ef45
poisontears69 <120147011+poisontears69@users.noreply.github.com>
2024-12-12 22:07:55 +0800

    updated the title

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ $patientCount = $stats->getPatientCount();
 
 <head>
     <meta charset="utf-8">
-    <title>Klinik - Clinic Website Template</title>
+    <title>Health Connect</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
